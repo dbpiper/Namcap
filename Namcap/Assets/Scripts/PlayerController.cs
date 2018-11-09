16cadff09dfe0366ea37876495ec8ce49445c901
David Piper <dbpiper@gmail.com>
2018-11-09 13:02:04 -0600

    Finish laying out level. Add player movement, begin adding collision to level.

---
+++ Namcap/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        public float Speed;
+        // Update is called once per frame
+        public void Update ()
+        {
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+            var y = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+
+            Debug.Log($"x: {x} and y: {y}");
+            var currentPos = transform.position;
+            var newPos = new Vector3(x, y, currentPos.z);
+            transform.Translate(newPos);
+        }
+    }
+}
